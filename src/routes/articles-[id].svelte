4105f3f530af903c9eb0a366619e00b7ba07c55e
karetnikov_dv <dkaretnikov@aeb-it.ru>
2022-05-15 20:13:17 +0900

    feat: add article page

---
+++ src/routes/articles-[id].svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Article from "../components/Article.svelte";
+    import { onMount } from "svelte";
+    import { page } from "$app/stores";
+
+    let articles = [
+        {
+            id: "1",
+            title: "Keyboard Cat",
+            imgsrc: "/sea.webp",
+            content:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, alias!",
+        },
+        {
+            id: "2",
+            title: "Maru",
+            imgsrc: "/jungle.webp",
+            content:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, alias!",
+        },
+        { id: "3", title: "Henri The Existential Cat", content: "content123" },
+    ];
+    let res: any;
+
+    let findArticleById = (id: any) => {
+        return articles.find((article: any) => article.id === id);
+    };
+    onMount(async () => {
+        res = findArticleById($page.params.id);
+    });
+</script>
+
+<div class="container">
+    <Article {...res} />
+</div>
+
+<style>
+</style>
